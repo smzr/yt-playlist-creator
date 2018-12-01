096481ddf8b3d76a869dce4eaf04919cca0c5839
Samazer <smzr@pm.me>
2018-12-01 15:44:12 +0000

    Seedling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import './bootstrap-grid.css'
+import queryString from 'query-string';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputTitle: '',
+        };
+
+        this.getInputData = this.getInputData.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.getAPIdata = this.getAPIdata.bind(this);
+    }
+
+    getInputData(title) {
+        this.setState({inputTitle: title})
+    }
+
+    getAPIdata() {
+        let parsed = queryString.parse(window.location.search);
+        let accessToken = parsed.access_token
+        fetch('https://www.googleapis.com/youtube/v3/playlists?part=snippet', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken,
+            },
+            body: JSON.stringify({
+                'snippet':
+                {
+                    'title':this.state.inputTitle
+                }
+            })
+        }).then(response => response.json()).then(data => {
+            console.log(data)
+            window.alert('https://www.youtube.com/playlist?list=' + data.id + '\n if this says undefined look in the console for errors (ctrl+shift+i)')
+        })
+    }
+
+    handleClick() {
+        this.getAPIdata(this.state.inputTitle)
+    }
+
+    render() {
+        return (<div className="App">
+        <h1>Playlist creator</h1>
+        <h2>YouTube</h2>
+        <button className="token-btn" onClick={() => window.location = 'https://yt-playlist-creator-backend.herokuapp.com/authenticate'}>Get Token</button>
+        <SongInput callback={this.getInputData} btnpress={this.handleClick}/>
+        <div className="row">
+        </div>
+    </div>);
+}
+}
+
+class SongInput extends Component {
+    render() {
+        return (<div className="searchbar">
+        <input type="text" placeholder="Playlist title" onChange={e => this.props.callback(e.target.value)}/>
+        <button className='submit' onClick={this.props.btnpress}>Create</button>
+    </div>);
+}
 }
 
 export default App;
